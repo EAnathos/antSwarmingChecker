008bab0891cf517d12fbcadfb1c0d43bea73766f
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-05-25 21:03:05 +0200

    dataSamples and dataLoader

---
+++ src/dataLoader.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+
+let speciesByRegion: Record<string, string[]> = {};
+let speciesByDate: Record<string, string[]> = {};
+
+export const loadData = () => {
+  const regionDataPath = path.join('./data', 'species_by_region.json');
+  const dateDataPath = path.join('./data', 'species_by_date.json');
+
+  const regionData = fs.readFileSync(regionDataPath, 'utf-8');
+  speciesByRegion = JSON.parse(regionData);
+
+  const dateData = fs.readFileSync(dateDataPath, 'utf-8');
+  speciesByDate = JSON.parse(dateData);
+};
+
+export const getSpeciesByRegion = (region: number): string[] => {
+  console.log(speciesByRegion[region])
+  return speciesByRegion[region] || [];
+};
+
+export const getSpeciesByDate = (month: string): string[] => {
+  return speciesByDate[month.toLowerCase()] || [];
+};
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import { SlashCommand } from './types';
 import { join } from 'path';
 import { readdirSync } from 'fs';
+import { loadData } from './dataLoader';
 
 dotenv.config();
 
@@ -22,5 +23,7 @@ const handlersDir = join(__dirname, "./handlers");
 readdirSync(handlersDir).forEach(handler => {
   require(`${handlersDir}/${handler}`)(client);
 });
+
+loadData();
 
 client.login(process.env.TOKEN);
