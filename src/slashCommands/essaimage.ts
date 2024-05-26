18380e9a9c7756d7ef5224c7cbb43b7c4344b78c
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-05-26 04:15:08 +0200

    Refactor and improve
    Need to be cleaned

--- src/slashCommands/essaimage.ts
+++ src/slashCommands/essaimage.ts
@@ -1,52 +1,135 @@
-import { SlashCommandBuilder, CommandInteractionOptionResolver } from 'discord.js';
-import { SlashCommand } from '../types';
-import { getSpeciesByRegion, getSpeciesByDate } from '../dataLoader';
+import {
+  SlashCommandBuilder,
+  CommandInteractionOptionResolver,
+} from "discord.js";
+import { SlashCommand } from "../types";
+import { getSpeciesByRegion, getSpeciesByDate } from "../dataLoader";
 
 const frenchMonths = [
-  "janvier", "février", "mars", "avril", "mai", "juin",
-  "juillet", "août", "septembre", "octobre", "novembre", "décembre"
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "août",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre",
 ];
 
 export const command: SlashCommand = {
-  name: 'essaimage',
+  name: "essaimage",
   data: new SlashCommandBuilder()
     .setName("essaimage")
     .setDescription("Liste les essaimages d'une région selon une date")
-    .addStringOption(option => // Adding option for region
-      option.setName("region")
-        .setDescription("Indiquer la région en question (en nombre, exemple: 01 pour Ain)")
-        .setRequired(true)
+    .addStringOption(
+      (
+        option // Adding option for region
+      ) =>
+        option
+          .setName("region")
+          .setDescription(
+            "Indiquer la région en question (en nombre, exemple: 01 pour Ain)"
+          )
+          .setRequired(false)
     )
-    .addStringOption(option => // Adding option for month
-      option.setName("mois")
-        .setDescription("Indiquer le mois en question (en lettre, exemple: janvier)")
-        .setRequired(false)
+    .addStringOption(
+      (
+        option // Adding option for month
+      ) =>
+        option
+          .setName("mois")
+          .setDescription(
+            "Indiquer le mois en question (en lettre, exemple: janvier)"
+          )
+          .setRequired(false)
     ),
   execute: async (interaction) => {
     const options = interaction.options as CommandInteractionOptionResolver;
 
     // Get month and region from the options
-    let month = options.getString("mois");
-    const location = options.getString("region")!;
+    let month = options.getString("mois")?.toLocaleLowerCase();
+    const location = options.getString("region");
 
-    // If no month is specified, take the current month
-    if (!month) {
-      const currentMonthIndex = new Date().getMonth();
-      month = frenchMonths[currentMonthIndex];
+    let message: string;
+
+    if (month) {
+      if (!frenchMonths.includes(month)) {
+        await interaction.reply("Veuillez indiquer un mois valide.");
+        return;
+      }
     }
 
-    // Get species for the specified region and month
-    const regionSpecies = getSpeciesByRegion(location);
-    const dateSpecies = getSpeciesByDate(month);
+    if (location) {
+      if (location.length !== 2) {
+        await interaction.reply(
+          "Veuillez indiquer un numéro de région valide."
+        );
+        return;
+      }
+    }
 
-    // Filter species present in both the region and the specified date
-    const speciesList = (regionSpecies.filter(species => dateSpecies.includes(species))).map(species => `* ${species}`).join('\n');
+    if (!location && !month) {
+      await interaction.reply("Veuillez indiquer une région ou un mois.");
+      return;
+    }
 
-    // Construct replied message
-    const message: string = `**Voici les essaimages pour le mois de \`${month}\` et le département numéro \`${location}\` :**\n` +
-                            (speciesList ? speciesList : "Aucune espèce trouvée.");
+    if (!location && month) {
+      const dateSpecies = getSpeciesByDate(month);
+      if (!dateSpecies) {
+        await interaction.reply(
+          `Aucun essaimage n'est prévu pour le mois de \`${month}\`.`
+        );
+        return;
+      }
 
-    // Reply to the interaction with the message
-    await interaction.reply(message);
-  }
+      await interaction.reply(
+        `Les essaimages prévus pour le mois de ${month} sont: ${dateSpecies!.join(
+          ", "
+        )}.`
+      );
+    } else if (!month && location) {
+      const regionSpecies = getSpeciesByRegion(location);
+      if (!regionSpecies) {
+        await interaction.reply(
+          `Aucun essaimage n'est prévu pour la région ${location}.`
+        );
+      }
+
+      await interaction.reply(
+        `Les essaimages prévus pour la région ${location} sont: ${regionSpecies!.join(
+          ", "
+        )}.`
+      );
+    } else {
+      const regionSpecies = getSpeciesByRegion(location);
+      const dateSpecies = getSpeciesByDate(month!);
+
+      if (!regionSpecies || !dateSpecies) {
+        await interaction.reply(
+          `Aucun essaimage n'est prévu pour la région ${location} et le mois de ${month}.`
+        );
+        return;
+      }
+
+      const species = regionSpecies.filter((value) =>
+        dateSpecies.includes(value)
+      );
+
+      if (species.length === 0) {
+        await interaction.reply(
+          `Aucun essaimage n'est prévu pour la région ${location} et le mois de ${month}.`
+        );
+      } else {
+        await interaction.reply(
+          `Les essaimages prévus pour la région ${location} et le mois de ${month} sont: ${species.join(
+            ", "
+          )}.`
+        );
+      }
+    }
+  },
 };
