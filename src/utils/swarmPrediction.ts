c9049d39686ef33f7327c87dfe8c9cda2a2d3694
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-05-28 22:53:41 +0200

    Update swarmPrediction.ts

--- src/utils/swarmPrediction.ts
+++ src/utils/swarmPrediction.ts
@@ -1,44 +1,70 @@
 import { getSpeciesByArea, getSpeciesByDate } from "../dataLoader";
 
+/**
+ * Predict the species that will swarm in a given month and area.
+ *
+ * @param {string} month - The name of the month.
+ * @param {string} area - The name of the area.
+ * @returns {string} The predicted species that will swarm.
+ */
 export function predictSwarming(month: string = "", area: string = ""): string {
-    if (month && area) return predictSwarmingByMonthAndArea(month, area)
-    else if (month) return predictSwarmingByMonth(month)
-    else return predictSwarmingByArea(area)
+  if (month && area) return predictSwarmingByMonthAndArea(month, area);
+  else if (month) return predictSwarmingByMonth(month);
+  else return predictSwarmingByArea(area);
 }
 
+/*
+ * Predict the species that will swarm in a given month and area.
+ *
+ * @param {string} month - The name of the month.
+ * @param {string} area - The name of the area.
+ * @returns {string} The predicted species that will swarm.
+ */
 function predictSwarmingByMonthAndArea(month: string, area: string) {
-    const areaSpecies = getSpeciesByArea(area);
-    const dateSpecies = getSpeciesByDate(month!);
-  
-    if (!areaSpecies || !dateSpecies)
-      return `Aucun essaimage n'est prévu pour le département \`${area}\` au mois de \`${month}\`.`;
-  
-    const species = areaSpecies.filter((value) => dateSpecies.includes(value));
-  
-    if (species.length === 0)
-      return `Aucun essaimage n'est prévu pour le département \`${area}\` au mois de \`${month}\`.`;
-  
-    return `**Les essaimages prévus pour le département \`${area}\` au mois de \`${month}\` sont :**\n* *${species.join(
-      "*\n* *"
-    )}*`;
-  }
-  
-  function predictSwarmingByMonth(month: string) {
-    const dateSpecies = getSpeciesByDate(month);
-    if (!dateSpecies)
-      return `Aucun essaimage n'est prévu pour le mois de \`${month}\`.`;
-  
-    return `**Les essaimages prévus pour le mois de \`${month}\` sont :**\n*  *${dateSpecies!.join(
-      "*\n* *"
-    )}*`;
-  }
-  
-  function predictSwarmingByArea(area: string) {
-    const areaSpecies = getSpeciesByArea(area);
-    if (!areaSpecies)
-      return `Aucune espèce n'essaime dans le département \`${area}\`.`;
-  
-    return `**Les essaimages possibles pour le département \`${area}\` sont :**\n*  *${areaSpecies!.join(
-      "*\n* *"
-    )}*`;
-  }
+  const areaSpecies = getSpeciesByArea(area);
+  const dateSpecies = getSpeciesByDate(month!);
+
+  if (!areaSpecies || !dateSpecies)
+    return `Aucun essaimage n'est prévu pour le département \`${area}\` au mois de \`${month}\`.`;
+
+  const species = areaSpecies.filter((value) => dateSpecies.includes(value));
+
+  if (species.length === 0)
+    return `Aucun essaimage n'est prévu pour le département \`${area}\` au mois de \`${month}\`.`;
+
+  return `**Les essaimages prévus pour le département \`${area}\` au mois de \`${month}\` sont :**\n* *${species.join(
+    "*\n* *"
+  )}*`;
+}
+
+/*
+  * Predict the species that will swarm in a given month.
+  *
+  * @param {string} month - The name of the month.
+  * @returns {string} The predicted species that will swarm.
+  */
+function predictSwarmingByMonth(month: string) {
+  const dateSpecies = getSpeciesByDate(month);
+  if (!dateSpecies)
+    return `Aucun essaimage n'est prévu pour le mois de \`${month}\`.`;
+
+  return `**Les essaimages prévus pour le mois de \`${month}\` sont :**\n*  *${dateSpecies!.join(
+    "*\n* *"
+  )}*`;
+}
+
+/*
+ * Predict the species that will swarm in a given area.
+ *
+ * @param {string} area - The name of the area.
+ * @returns {string} The predicted species that will swarm.
+ */
+function predictSwarmingByArea(area: string) {
+  const areaSpecies = getSpeciesByArea(area);
+  if (!areaSpecies)
+    return `Aucune espèce n'essaime dans le département \`${area}\`.`;
+
+  return `**Les essaimages possibles pour le département \`${area}\` sont :**\n*  *${areaSpecies!.join(
+    "*\n* *"
+  )}*`;
+}
