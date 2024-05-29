0b41b505d3b1fe46fe42f67c692698175555fe65
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-05-29 23:21:56 +0200

    Create antarea.ts
    This command shows information on the Antarea Association

---
+++ src/slashCommands/antarea.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { SlashCommand } from "../types";
+
+export const command: SlashCommand = {
+  name: "antarea",
+  data: new SlashCommandBuilder()
+    .setName("antarea")
+    .setDescription("Affiche des informations sur l'association Antarea"),
+  execute: async (interaction) => {
+    const antareaPresentationEmbed = new EmbedBuilder()
+      .setTitle("Antarea")
+      .setURL("https://antarea.fr")
+      .setDescription(
+        `Cette association a pour but de réaliser des études et des identifications pour mieux connaitre les espèces, la répartition et la localisation des fourmis françaises métropolitaines
+        
+        Je ne peux que renseigner de vous renseigner sur cette association qui a une réelle importance pour nous myrmécophiles en herbe !`
+      )
+      .setThumbnail(
+        "https://antarea.fr/fourmi/public/themes/antarea/img/antarealogo.png"
+      )
+      .setColor("#3AB0E8");
+
+    await interaction.reply({
+      embeds: [antareaPresentationEmbed],
+      ephemeral: true,
+    });
+  },
+};
