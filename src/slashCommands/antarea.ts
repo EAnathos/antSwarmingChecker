1f5890b78c7cffe672318e33386f7bae0718e97e
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-12-21 15:47:00 +0100

    update: fix a french error

--- src/slashCommands/antarea.ts
+++ src/slashCommands/antarea.ts
@@ -13,7 +13,7 @@ export const command: SlashCommand = {
       .setDescription(
         `Cette association a pour but de réaliser des études et des identifications pour mieux connaitre les espèces, la répartition et la localisation des fourmis françaises métropolitaines
         
-        Je ne peux que renseigner de vous renseigner sur cette association qui a une réelle importance pour nous myrmécophiles en herbe !`
+        Je ne peux que vous conseiller de vous renseigner sur cette association qui a une réelle importance pour nous myrmécophiles en herbe !`
       )
       .setThumbnail(
         "https://antarea.fr/fourmi/public/themes/antarea/img/antarealogo.png"
